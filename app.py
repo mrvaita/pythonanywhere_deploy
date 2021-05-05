79033060215f22a0ddfcedeb789487688c336ab3
mrvaita <michele.tassoni@gmail.com>
2021-05-05 11:22:16 +0200

    fixed indentation and changed message

--- app.py
+++ app.py
@@ -6,17 +6,19 @@ import git
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello_world():
-    return 'Hello from Flask!'
+    return 'Hello from Flask Michele!'
+
 
 @app.route("/")
 @app.route('/update_server', methods=['POST'])
-    def webhook():
-        if request.method == 'POST':
-            repo = git.Repo('.')
-            origin = repo.remotes.originorigin.pull()
+def webhook():
+    if request.method == 'POST':
+        repo = git.Repo('.')
+        origin = repo.remotes.originorigin.pull()
             
-            return 'Updated PythonAnywhere successfully', 200
-        else:
-            return 'Wrong event type', 400
+        return 'Updated PythonAnywhere successfully', 200
+    else:
+        return 'Wrong event type', 400
