fa3e97794bb9bea89d71bb4e28536566ceb5fef1
mrvaita <michele.tassoni@gmail.com>
2021-05-05 09:18:41 +0000

    added flask app and gitignore

---
+++ app.py
@@ -0,0 +1,22 @@
+
+# A very simple Flask Hello World app for you to get started with...
+
+from flask import Flask
+import git
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello from Flask!'
+
+@app.route("/")
+@app.route('/update_server', methods=['POST'])
+    def webhook():
+        if request.method == 'POST':
+            repo = git.Repo('.')
+            origin = repo.remotes.originorigin.pull()
+            
+            return 'Updated PythonAnywhere successfully', 200
+        else:
+            return 'Wrong event type', 400
